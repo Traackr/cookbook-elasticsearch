1aee8fd89b3bee3fc4d9cabe9eba04e9cd20acaf
George P. Stathis <george@george-stathis.com>
2013-01-30 16:47:14 -0500

    Fixing parameter expansion issue

--- attributes/custom_jar.rb
+++ attributes/custom_jar.rb
@@ -1,11 +1,6 @@
-# Load configuration and credentials from data bag 'elasticsearch/custom_jar' -
-#
-aws = Chef::DataBagItem.load('elasticsearch', 'custom_jar') rescue {}
-# ----------------------------------------------------------------------
-
 # === CUSTOM JAR ===
 #
-default.elasticsearch[:custom_jar][:name] = "none";
-default.elasticsearch[:custom_jar][:url] = "none";
+default.elasticsearch[:custom_jar][:name] = nil;
+default.elasticsearch[:custom_jar][:url] = nil;
 default.elasticsearch[:custom_jar][:url_credential_user] = nil;
 default.elasticsearch[:custom_jar][:url_credential_pwd] = nil;
--- recipes/custom_jar.rb
+++ recipes/custom_jar.rb
@@ -5,6 +5,6 @@ execute "Get custum Jar" do
   command "curl -L -o #{node.elasticsearch[:dir]}/elasticsearch/lib/#{node.elasticsearch[:custom_jar][:name]} -u #{node.elasticsearch[:custom_jar][:url_credential_user]}:#{node.elasticsearch[:custom_jar][:url_credential_pwd]} '#{node.elasticsearch[:custom_jar][:url]}'"
 	user node.elasticsearch[:user]
 	group node.elasticsearch[:user]
-	not_if (node.elasticsearch[:jar_name]=="none") 
+	not_if (node.elasticsearch[:custom_jar][:name]==nil) 
 	notifies :restart, resources(:service => 'elasticsearch')	
 end
