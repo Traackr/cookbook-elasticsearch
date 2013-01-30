f3186b23514b885bd84108cd601610c79da916a8
George P. Stathis <george@george-stathis.com>
2013-01-30 16:21:09 -0500

    Installing custom JAR as a separate recipe

---
+++ attributes/custom_jar.rb
@@ -0,0 +1,6 @@
+# === CUSTOM JAR ===
+#
+default.elasticsearch[:custom_jar][:name] = "none";
+default.elasticsearch[:custom_jar][:url] = "none";
+default.elasticsearch[:custom_jar][:url_credential_user] = nil;
+default.elasticsearch[:custom_jar][:url_credential_pwd] = nil;
---
+++ recipes/custom_jar.rb
@@ -0,0 +1,10 @@
+# Install custom jar
+#
+
+execute "Get custum Jar" do
+  command "curl -L -o #{node.elasticsearch[:dir]}/elasticsearch/lib/#{node.elasticsearch[:custom_jar][:name]} -u #{node.elasticsearch[:custom_jar][:url_credential_user]}:#{node.elasticsearch[:custom_jar][:url_credential_pwd]} '#{node.elasticsearch[:custom_jar][:url]}'"
+	user node.elasticsearch[:user]
+	group node.elasticsearch[:user]
+	not_if (node.elasticsearch[:jar_name]=="none") 
+	notifies :restart, resources(:service => 'elasticsearch')	
+end
