749418ca40bb2106d58e9f7bd6e55fe098626dfc
Bryan Warner <bryan.warner@gmail.com>
2013-02-25 15:25:08 -0500

    Updating elasticsearch custom_jar recipe

--- attributes/custom_jar.rb
+++ attributes/custom_jar.rb
@@ -1,6 +1,3 @@
 # === CUSTOM JAR ===
 #
-default.elasticsearch[:custom_jar][:name] = nil;
-default.elasticsearch[:custom_jar][:url] = nil;
-default.elasticsearch[:custom_jar][:url_credential_user] = nil;
-default.elasticsearch[:custom_jar][:url_credential_pwd] = nil;
+default.elasticsearch[:custom_jars] = [];
--- recipes/custom_jar.rb
+++ recipes/custom_jar.rb
@@ -1,10 +1,14 @@
 # Install custom jar
 #
 
-execute "Get custum Jar" do
-  command "curl -L -o #{node.elasticsearch[:dir]}/elasticsearch/lib/#{node.elasticsearch[:custom_jar][:name]} -u #{node.elasticsearch[:custom_jar][:url_credential_user]}:#{node.elasticsearch[:custom_jar][:url_credential_pwd]} '#{node.elasticsearch[:custom_jar][:url]}'"
-	user node.elasticsearch[:user]
-	group node.elasticsearch[:user]
-	not_if (node.elasticsearch[:custom_jar][:name]==nil) 
-	notifies :restart, resources(:service => 'elasticsearch')	
+unless "#{node.elasticsearch[:custom_jars]}".empty?
+    "#{node.elasticsearch[:custom_jars]}".each do |custom_jar|
+        execute "Get custom Jar" do
+            command "curl -L -o #{node.elasticsearch[:dir]}/elasticsearch/lib/#{custom_jar['name']} -u #{custom_jar['url_credential_user']}:#{custom_jar['url_credential_pwd']} #{custom_jar['url']}"
+	        user node.elasticsearch[:user]
+	        group node.elasticsearch[:user]
+	        not_if (custom_jar['name']==nil) 
+	        notifies :restart, resources(:service => 'elasticsearch')	
+        end
+    end
 end
