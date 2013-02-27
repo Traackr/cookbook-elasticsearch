c9b39913c6937c1cc8c414afe31c0b049fcbbef1
Bryan Warner <bryan.warner@gmail.com>
2013-02-27 09:44:49 -0500

    Updating custom jar recipe

--- recipes/custom_jar.rb
+++ recipes/custom_jar.rb
@@ -1,8 +1,8 @@
 # Install custom jar
 #
 
-unless "#{node.elasticsearch[:custom_jars]}".empty?
-    "#{node.elasticsearch[:custom_jars]}".each do |custom_jar|
+unless node.elasticsearch[:custom_jars].empty?
+    node.elasticsearch[:custom_jars].each do |custom_jar|
         execute "Get custom Jar" do
             command "curl -L -o #{node.elasticsearch[:dir]}/elasticsearch/lib/#{custom_jar['name']} -u #{custom_jar['url_credential_user']}:#{custom_jar['url_credential_pwd']} #{custom_jar['url']}"
 	        user node.elasticsearch[:user]
