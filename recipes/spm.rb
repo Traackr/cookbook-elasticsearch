d603e2cec972a45bfa7ef0b7e0f0bf0ba6b301ff
George P. Stathis <george@george-stathis.com>
2013-08-03 17:31:46 -0400

    Fixing SPM settings

--- recipes/spm.rb
+++ recipes/spm.rb
@@ -22,3 +22,9 @@ execute "Install SPM for #{node.elasticsearch[:spm][:key]}" do
   command "curl -ko installer.sh \"https://apps.sematext.com/spm-reports/installerDownload.do?client_token=#{node.elasticsearch[:spm][:key]}\" && sudo bash installer.sh"
   not_if (node.elasticsearch[:spm]==nil)
 end
+
+execute "Configure SPM for #{node.elasticsearch[:spm][:key]}" do
+  user 'root'
+  command "bash #{node.elasticsearch[:spm][:path]}/bin/spm-client-setup-conf.sh #{node.elasticsearch[:spm][:key]} es javaagent"
+  not_if (node.elasticsearch[:spm]==nil)
+end
